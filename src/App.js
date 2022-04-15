1ee4c7c1445b986dee5e633d74ab1bff0df39f41
Teodor1331 <teodor.sdishanski@gmail.com>
2022-04-15 16:36:39 +0300

    Add Simple Username Form

--- src/App.js
+++ src/App.js
@@ -3,13 +3,36 @@ import TrelloColumn from "./Components/TrelloColumn";
 
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="App">
-        <TrelloColumn title="test"/>
+        <form onSubmit={this.handleSubmit}>
+        <label>
+          Username: 
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
       </div>
-    );
+    )
   }
 }
 
