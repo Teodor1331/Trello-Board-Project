fad0e0bae57f351e668d05ed646506baa9493cb8
Teodor1331 <teodor.sdishanski@gmail.com>
2022-04-17 02:13:22 +0300

    Update Database Schema, Fix Bugs in Authentication

--- src/Database.js
+++ src/Database.js
@@ -5,8 +5,9 @@ export const dataBase = new Dexie('TrelloDatabase');
 // https://dexie.org/docs/Version/Version.stores()
 
 dataBase.version(3).stores({
-    users: '++id, &username, &password, isLogged',
+    users: '++id, &username, password',
     boards: '++id, &title, user_id',
-    columns: '++id, &title, index, board_id',
-    cards: '++id, &title, description, user_id, column_id, board_id'
+    columns: '++id, title, index, board_id',
+    cards: '++id, &title, description, user_id, column_id, board_id',
+    recent: '++id, [user_id+board_id]'
 })
--- src/Pages/ErrorPage.js
+++ src/Pages/ErrorPage.js
@@ -2,16 +2,14 @@ import React from "react";
 
 import { Container } from "react-bootstrap";
 
-class ErrorPage extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <Container className = {'my-6'}>
-                    <h1>You entered an invalid address!</h1>
-                </Container>
-            </React.Fragment>
-        )
-    }
+const ErrorPage = () => {
+    return (
+        <React.Fragment>
+            <Container className = 'my-6'>
+                <h1>You entered an invalid URL address!</h1>
+            </Container>
+        </React.Fragment>
+    )
 }
 
 export default ErrorPage;
