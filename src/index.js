c0d7b7e5af8137125762401b35ea29584d397bee
Teodor1331 <teodor.sdishanski@gmail.com>
2022-04-16 09:51:38 +0300

    Simplify index.js + Add React Router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from "react-redux"
+
 
 import './index.css';
 import App from './App';
-import store from './Store'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>);
