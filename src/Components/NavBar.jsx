c73773474f796498294eb9e00a1ae6dac1f8c7c8
Vic-Dim <victor.dimitrov2003@gmail.com>
2022-04-16 10:51:37 +0300

    Adding navbar template and routes file

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react"
+
+class NavBar extends Component {
+    state = {  } 
+    render() { 
+        return (<button>Hello darkness my old friend</button>);
+    }
+}
+ 
+export default NavBar;
