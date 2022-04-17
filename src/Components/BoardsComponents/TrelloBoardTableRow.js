708295180298dea3e166ce3530d73a867485122c
unknown <ivanenchev2906@gmail.com>
2022-04-17 17:47:47 +0300

    Added ability to change board name

--- src/Components/BoardsComponents/TrelloBoardTableRow.js
+++ src/Components/BoardsComponents/TrelloBoardTableRow.js
@@ -11,6 +11,11 @@ const TrelloBoardTableRow = (props) => {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    let newName = '';
+    const handleChange = (event) => {
+        const value = event.target.value;
+        newName = value;
+    }
 
     return (
         <React.Fragment>
@@ -18,7 +23,7 @@ const TrelloBoardTableRow = (props) => {
                 <p>{props.board.title}</p>
                 <button onClick = {() => props.onDelete(props.board.id)}>Delete</button>
                 <Button variant="primary" onClick={handleShow}>
-                    Update board
+                    Update
                 </Button>
 
                 <Modal show={show} onHide={handleClose}>
@@ -29,7 +34,7 @@ const TrelloBoardTableRow = (props) => {
                     <Form>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                             <Form.Label>New name</Form.Label>
-                            <Form.Control as="textarea" />
+                            <input type="text" name="newName" onChange={handleChange}/>
                         </Form.Group>
                     </Form>
                     </Modal.Body>
@@ -37,7 +42,7 @@ const TrelloBoardTableRow = (props) => {
                     <Button variant="secondary" onClick={handleClose}>
                         Close
                     </Button>
-                    <Button variant="primary" onClick={() => props.onUpdate(props.board.id, "lmaooo")}>
+                    <Button variant="primary" onClick={() => props.onUpdate(props.board.id, newName)}>
                         Save Changes
                     </Button>
                     </Modal.Footer>
