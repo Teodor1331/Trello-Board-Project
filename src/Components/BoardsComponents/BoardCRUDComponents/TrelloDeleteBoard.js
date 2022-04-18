b30c5303650cbea6486dc83f6d629fff4cb39b04
Vic-Dim <victor.dimitrov2003@gmail.com>
2022-04-18 17:47:41 +0300

    Deleting useless Board CRUD files

--- src/Components/BoardsComponents/BoardCRUDComponents/TrelloDeleteBoard.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const TrelloDeleteBoard = () => {
-    return (
-        <div></div>
-    )
-}
-
-export default TrelloDeleteBoard;
--- src/Components/BoardsComponents/BoardCRUDComponents/TrelloUpdateBoard.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const TrelloUpdateBoard = () => {
-    return (
-        <div></div>
-    )
-}
-
-export default TrelloUpdateBoard;
